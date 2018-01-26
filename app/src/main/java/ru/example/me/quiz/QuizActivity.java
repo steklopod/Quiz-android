b72d11446283eeb1f60b9398a3a718f8a08d61db
Dmitry.K <steklopod@gmail.com>
2018-01-26 21:50:31 +0300

    Сохранение текущего индекса вопроса в Bundle при повороте телефона.

--- app/src/main/java/ru/example/me/quiz/QuizActivity.java
+++ app/src/main/java/ru/example/me/quiz/QuizActivity.java
@@ -2,18 +2,27 @@ package ru.example.me.quiz;
 
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
+import android.widget.ImageButton;
 import android.widget.TextView;
 import android.widget.Toast;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
 public class QuizActivity extends AppCompatActivity {
+    private static Logger logger = LoggerFactory.getLogger(QuizActivity.class);
 
     private Button mTrueButton;
     private Button mFalseButton;
-    private Button mNextButton;
-    private Button mPreviosButton;
+    private ImageButton mNextButton;
+    private ImageButton mPreviosButton;
     private TextView mQuestionTextView;
+
+    private static final String TAG = "QuizActivity";
+    private static final String KEY_INDEX = "index";
 
     private Question[] mQuestions = new Question[]{
             new Question(R.string.q_afraid, false),
@@ -27,6 +36,12 @@ public class QuizActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        Log.d(TAG, "onCreate(Bundle) called");
+        if (savedInstanceState != null) {
+            mCurrentIndex = savedInstanceState.getInt(KEY_INDEX, 0);
+            logger.info("mCurrentIndex = " + String.valueOf(mCurrentIndex));
+        }
+
         setContentView(R.layout.activity_quiz);
 
         mQuestionTextView = (TextView) findViewById(R.id.question_text_view);
@@ -55,28 +70,28 @@ public class QuizActivity extends AppCompatActivity {
             }
         });
 
-        mNextButton = (Button) findViewById(R.id.next_button);
+        mNextButton = (ImageButton) findViewById(R.id.next_button);
         mNextButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
                 nextQuestion();
             }
         });
-        mPreviosButton = (Button) findViewById(R.id.prev_button);
+        mPreviosButton = (ImageButton) findViewById(R.id.prev_button);
         mPreviosButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
                 if (mCurrentIndex != 0) {
                     mCurrentIndex = (mCurrentIndex - 1);
                 } else {
-                    mCurrentIndex = mQuestions.length-1;
+                    mCurrentIndex = mQuestions.length - 1;
                 }
                 updateQuestion();
             }
         });
     }
 
-    private void nextQuestion(){
+    private void nextQuestion() {
         mCurrentIndex = (mCurrentIndex + 1) % mQuestions.length;
         updateQuestion();
     }
@@ -97,4 +112,45 @@ public class QuizActivity extends AppCompatActivity {
         }
         Toast.makeText(this, messageResId, Toast.LENGTH_SHORT).show();
     }
+
+    @Override
+    protected void onStart() {
+        super.onStart();
+        Log.d(TAG, "onStart(Bundle) called");
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+        Log.d(TAG, "onResume(Bundle) called");
+    }
+
+    @Override
+    protected void onPause() {
+        super.onPause();
+        Log.d(TAG, "onPause(Bundle) called");
+    }
+
+    @Override
+    protected void onStop() {
+        super.onStop();
+        Log.d(TAG, "onStop(Bundle) called");
+    }
+
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+        Log.d(TAG, "onDestroy(Bundle) called");
+    }
+
+    @Override
+    protected void onSaveInstanceState(Bundle savedInstanceState) {
+        super.onSaveInstanceState(savedInstanceState);
+        Log.i(TAG, "savedInstanceState");
+        savedInstanceState.putInt(KEY_INDEX, mCurrentIndex);
+    }
+
+    /**
+     * 89 стр
+     */
 }
